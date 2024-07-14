8f70db2989c90782b05b55b61424c7eb2e3ae64f
McNugget <alexd84a@gmail.com>
2024-07-14 22:14:37 +0200

    d1 updated with part description comments.

--- src/calendar/day01.zig
+++ src/calendar/day01.zig
@@ -10,7 +10,13 @@ pub fn run(skip: bool) !void {
         return;
 
     try safePrint("Day 1.\n", .{});
+
+    // On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+    // Consider your entire calibration document. What is the sum of all of the calibration values?
     try partOne(false);
+
+    // It looks like some of the digits are actually spelled out with letters: one, two ...
+    // you now need to find the real first and last digit on each line
     try partTwo(false);
 }
 
